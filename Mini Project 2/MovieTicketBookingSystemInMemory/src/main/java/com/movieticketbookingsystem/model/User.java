ebcbdddb3d82baf46ff1873e7ed1ddc001bf1703
SatishSahu0307 <satishsahu.satishsahu@gmail.com>
2025-07-02 22:48:37 +0530

    Create User.java

---
+++ Mini Project 2/MovieTicketBookingSystemInMemory/src/main/java/com/movieticketbookingsystem/model/User.java
@@ -0,0 +1,103 @@
+package com.movieticketbookingsystem.model;
+
+import java.util.Objects;
+
+/**
+ * Represents a User in the Movie Ticket Booking System.
+ */
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String role; // 'admin', 'user'
+
+    // Constructors
+    public User() {
+    }
+
+    public User(int userId, String username, String password, String name, String email, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    // Getters
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    // Setters
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // toString, equals, hashCode
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+}
